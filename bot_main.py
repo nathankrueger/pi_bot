20a4740cd4a1681f1bee73e96b313147e51eaca5
Nathan Krueger <natekrueger805@gmail.com>
2016-02-02 21:06:13 -0800

    Changing interactive parameter

--- bot_main.py
+++ bot_main.py
@@ -27,7 +27,7 @@ def getArgs():
 	# Grab command-line arguments
 	parser = argparse.ArgumentParser(description='')
 	parser.add_argument('--port', type=int, help='Set the port to run the server on.')
-	parser.add_argument('--interactive', type=str, help='Control robot interactively.')
+	parser.add_argument('--interactive', action='store_true', help='Control robot interactively.')
 
 	return parser.parse_args()
 
